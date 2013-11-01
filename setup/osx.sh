2df2cbfb3b441da3994b5e6c3a0b7158454930d4
phette23 <phette23@gmail.com>
2013-11-01 11:49:30 -0400

    osx.sh: disable inline attachments in Mail stolen from mathiasbynens/dotfiles@40249d0d0ff795b7ca89263e15dccd1c0b855466

--- setup/osx.sh
+++ setup/osx.sh
@@ -408,6 +408,14 @@ defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
 # Add the keyboard shortcut ⌘ + Enter to send an email in Mail.app
 defaults write com.apple.mail NSUserKeyEquivalents -dict-add "Send" "@\\U21a9"
 
+# Display emails in threaded mode, sorted by date (oldest at the top)
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "DisplayInThreadedMode" -string "yes"
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortedDescending" -string "yes"
+defaults write com.apple.mail DraftsViewerAttributes -dict-add "SortOrder" -string "received-date"
+
+# Disable inline attachments (just show the icons)
+defaults write com.apple.mail DisableInlineAttachmentViewing -bool true
+
 ###############################################################################
 # Spotlight                                                                   #
 ###############################################################################

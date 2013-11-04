4ac6551d3773d97cf342ebab81fe384bac1ebbac
phette23 <phette23@gmail.com>
2013-11-04 01:58:33 -0500

    osx.sh: install XCode developer's tools

--- setup/osx.sh
+++ setup/osx.sh
@@ -7,11 +7,14 @@
 # exit if not OS X
 [[ $(uname) == 'Darwin' ]] || exit 1
 
+# Install XCode developer tools
+xcode-select --install
+
 # Some tools look for XCode, even though they don't need it.
 # https://github.com/joyent/node/issues/3681
 # https://github.com/mxcl/homebrew/issues/10245
 if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
-  sudo xcode-select -switch /usr/bin
+  sudo xcode-select -switch /
 fi
 
 # Install Homebrew.

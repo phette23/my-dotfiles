64fc8a07da2277009c8d3bee695a9d781f1ac898
phette23 <phette23@gmail.com>
2014-01-18 13:02:31 -0500

    mac: add `tab` command to open new terminal tabs

--- inc/mac.sh
+++ inc/mac.sh
@@ -90,3 +90,43 @@ EOF
 pdfman () {
     man -t "$*" | open -f -a /Applications/Preview.app
 }
+
+# tab $CMD opens a new tab & executes $CMD in it
+# stolen from oh-my-zsh's OS X plugin:
+# https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/osx/osx.plugin.zsh
+tab () {
+  local command="clear"
+  (( $# > 0 )) && command="$*"
+
+  the_app=$(
+    osascript 2>/dev/null <<EOF
+      tell application "System Events"
+        name of first item of (every process whose frontmost is true)
+      end tell
+EOF
+  )
+
+  [[ "$the_app" == 'Terminal' ]] && {
+    osascript 2>/dev/null <<EOF
+      tell application "System Events"
+        tell process "Terminal" to keystroke "t" using command down
+        tell application "Terminal" to do script "${command}" in front window
+      end tell
+EOF
+  }
+
+  [[ "$the_app" == 'iTerm' ]] && {
+    osascript 2>/dev/null <<EOF
+      tell application "iTerm"
+        set current_terminal to current terminal
+        tell current_terminal
+          launch session "Default Session"
+          set current_session to current session
+          tell current_session
+            write text "${command}"
+          end tell
+        end tell
+      end tell
+EOF
+  }
+}
